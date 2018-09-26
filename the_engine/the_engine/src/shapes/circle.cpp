c814a5a2e8779cfb16715153ecbcc3159b844910
Mitali Sengupta <mitali.s.auger@gmail.com>
2018-09-25 19:19:59 -0700

    Added files for shapes - Circle

---
+++ the_engine/the_engine/src/shapes/circle.cpp
@@ -0,0 +1,25 @@
+#include "shapes.h"
+
+void drawcircle(float radius, int sides)
+{
+
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	float twopi = (2.0f * 3.14159265358979f);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glBegin(GL_TRIANGLE_FAN);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex2f(0.0f, 0.0f);
+
+	for (int i = 0; i <= 360; i++)
+	{
+		glVertex2f((radius / 1.7)*cosf(i*twopi / sides), radius *sinf((i*twopi) / sides ));
+	}
+	glEnd();
+	glFlush();
+
+}
+ 
--- the_engine/the_engine/src/shapes/shapes.h
+++ the_engine/the_engine/src/shapes/shapes.h
@@ -1,1 +1,6 @@
 #pragma once
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <Math.h>
+
+void drawcircle(float radius, int sides);
