6e1adf647d6c64a901f0a36c9fe22739d20f78fb
Kevin Yook <yook00627@gmail.com>
2018-09-27 22:59:43 -0700

    delete test files

--- the_engine/the_engine/main.cpp
+++ the_engine/the_engine/main.cpp
@@ -2,7 +2,7 @@
 #include <math.h>
 #include "src/window/window.h"
 #include "src/shapes/shapes.h"
-#include "src/math/vec2.h"
+
 
 
 int main(int argc, char *argv)
@@ -10,15 +10,14 @@ int main(int argc, char *argv)
 	using namespace engine;
 	using namespace graphics;
 	using namespace input;
-	using namespace math;
+
 
 	
 	Window window(SCREEN_NAME, SCREEN_WIDTH, SCREEN_HEIGHT);
 	glClearColor(0.3f, 0.3f, 0.3f, 0.0f);
 	KeyboardMouse input_keys;
 	GLuint var;
-	vec2 vector1;
-	vec2 vector2(2, 3);
+
 
 
 	glGenVertexArrays(1, &var);
@@ -27,14 +26,12 @@ int main(int argc, char *argv)
 	while (!window.closed())
 	{
 		window.clear();
-#if 0
 		//drawcircle(.4f, 40);
 		//Triangle tri(10, 10);
 		//tri.Create_Triangle();
 		Square r(10);
 		r.draw_rectangle();
-		//vector.add(vec2(2, 3));
-		std::cout << (vector1 != vector2) << std::endl;
+
 		if (input_keys.key_pressed(GLFW_KEY_SPACE))
 		{
 			std::cout << "pressed space" << std::endl;
@@ -46,7 +43,7 @@ int main(int argc, char *argv)
 		double x, y;
 		input_keys.get_mouse_position(x, y);
 		std::cout << "x postion: " << x << " y position: " << y << std::endl;
-#endif
+
 		window.update();
 	}
 
--- the_engine/the_engine/src/utility/utility.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include "shader.h"
-#include "texture.h"
